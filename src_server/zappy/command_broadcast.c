8276c0575cbcea5b4a9afc919a4cde3c766682d8
LEONOV <serguei.leonov@epitech.eu>
2022-06-10 15:00:43 +0200

    [up] command_broadcast.c: send msg

--- src_server/zappy/command_broadcast.c
+++ src_server/zappy/command_broadcast.c
@@ -6,6 +6,7 @@
 */
 
 #include "server.h"
+#include <stdio.h>
 
 int get_pos(int a, int b, int map_size)
 {
@@ -20,8 +21,8 @@ int get_pos(int a, int b, int map_size)
 
 int find_path(trantorians_t *src, trantorians_t *dest, zappy_data_t *data)
 {
-	int x_dest = gest_pos(src->tile->x, dest->tile->x, data->map->height);
-	int y_dest = gest_pos(src->tile->y, dest->tile->y, data->map->width);
+	int x_dest = get_pos(src->tile->x, dest->tile->x, data->map->width);
+	int y_dest = get_pos(src->tile->y, dest->tile->y, data->map->height);
 	float c_dir;
 	
 	if (x_dest == 0)
@@ -29,7 +30,6 @@ int find_path(trantorians_t *src, trantorians_t *dest, zappy_data_t *data)
 	c_dir = y_dest / x_dest;
 	if (c_dir < 0)
 		c_dir *= -1;
-	printf("X: %f, Y: %f, dir: %f\n", x_dest, y_dest, c_dir);
 	if (x_dest > 0 && (y_dest == x_dest))
 		return 6;
 	if (x_dest < 0 && (y_dest == x_dest))
@@ -57,15 +57,18 @@ int find_path(trantorians_t *src, trantorians_t *dest, zappy_data_t *data)
 	return 0;
 }
 
-int send_broadcast(trantorians_t *trant, int i, int j, zappy_data_t *data)
+int send_broadcast(trantorians_t *trant, tile_t *tile, zappy_data_t *data, char *msg)
 {
-	trantorians_t *current = data->map->tiles[i][j]->trantorians;
+	trantorians_t *current = tile->trantorians;
+	char buff[512];
 	int dir;
 
 	while (current != NULL) {
 		dir = find_path(trant, current, data);
-		dir = (dir + current->direction * 3) % 8 + 1;
-		// current->client
+		if (dir)
+			dir = (dir + current->direction * 3) % 8 + 1;
+		sprintf(buff, "Received [%s] from [%d].\n", msg, dir);
+		current->client->data_send = add_send(current->client->data_send, buff);
 		current = current->next;
 	}
 	return 0;
@@ -76,10 +79,8 @@ int broadcast(trantorians_t *trant, char **args, zappy_data_t *data)
 	int width = data->map->width;
 	int height = data->map->height;
 
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
-			send_broadcast(trant, i, j, data);
-		}
-	}
+	for (int i = 0; i < height; i++)
+		for (int j = 0; j < width; j++)
+			send_broadcast(trant, data->map->tiles[i][j], data, args[0]);
 	return 0;
 }
