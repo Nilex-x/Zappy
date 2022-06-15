8b9f3214ac084fa504bb63b67d55a388ca8e38d9
Jules <jules.cibien@epitech.eu>
2022-06-15 17:59:00 +0200

    [up] change return

--- src_server/server/manage_incantation.c
+++ src_server/server/manage_incantation.c
@@ -11,6 +11,6 @@
 int incantation(trantorians_t *trant, char **arg, zappy_data_t *data)
 {
     if (check_incantation(data->map, trant) == -1)
-        return (1);
-    return (0);
+        return (0);
+    return (1);
 }
