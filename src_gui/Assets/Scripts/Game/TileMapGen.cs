b930cd70ac47fac1452e4d576ab41ce1b33f91da
Jules-Cibien <jules.cibien@epitech.eu>
2022-06-15 16:33:28 +0200

    [add] TileMap Gen

---
+++ src_gui/Assets/Scripts/Game/TileMapGen.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMapGen : MonoBehaviour
+{
+    public GameObject tilePrefab;
+
+    [SerializeField]int mapWidth = 5;
+    [SerializeField]int mapHeight = 5;
+
+    [SerializeField]float tileOffset = 1.45f;
+
+    void Start()
+    {
+        CreateTileMap();
+    }
+
+    void CreateTileMap()
+    {
+        for (int x = 0; x <= mapWidth; x++) {
+            for (int z = 0; z <= mapHeight; z++) {
+                GameObject Temp = Instantiate(tilePrefab);
+                Temp.transform.position = new Vector3(x * tileOffset, 0, z * tileOffset);
+                SetTileInfo(Temp, x, z);
+            }
+        }
+    }
+
+    void SetTileInfo(GameObject Temp, int x, int z)
+    {
+        Temp.transform.parent = transform;
+        Temp.name = x.ToString() + ", " + z.ToString();
+    }
+}
