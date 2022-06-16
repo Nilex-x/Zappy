eae2921bf13af99ab0b2e1abb15a07c9afbd4c1c
LEONOV <serguei.leonov@epitech.eu>
2022-06-16 11:14:05 +0200

    [up] main.py: add needed ressources for lvl up

--- src_client/ia/main.py
+++ src_client/ia/main.py
@@ -13,6 +13,56 @@ import getopt
 from sys import *
 
 
+# ---------------------- NEEDED ----------------------
+
+up2 = {
+    "player": 1,
+    "linemate": 1
+}
+up3 = {
+    "player": 2,
+    "linemate": 1,
+    "deraumere": 1,
+    "sibur": 1
+}
+up4 = {
+    "player": 2,
+    "linemate": 2,
+    "sibur": 1,
+    "phiras": 1
+}
+up5 = {
+    "player": 4,
+    "linemate": 1,
+    "deraumere": 1,
+    "sibur": 2,
+    "phiras": 1
+}
+up6 = {
+    "player": 4,
+    "linemate": 1,
+    "deraumere": 2,
+    "sibur": 1,
+    "mendiane": 3
+}
+up7 = {
+    "player": 6,
+    "linemate": 1,
+    "deraumere": 2,
+    "sibur": 3,
+    "phiras": 1
+}
+up8 = {
+    "player": 6,
+    "linemate": 2,
+    "deraumere": 2,
+    "sibur": 2,
+    "mendiane": 2,
+    "phiras": 2,
+    "thystame": 1
+}
+
+
 # ---------------------- CLIENT AI ----------------------
 
 class clientIA:
