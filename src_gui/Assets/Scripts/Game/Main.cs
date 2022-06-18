1d69c0379ac46a5be0af0960fe2f0684929b4bd0
Remy <remy.noulin@epitech.eu>
2022-06-18 23:42:23 +0200

    [ADD] mineral generation and update

--- src_gui/Assets/Scripts/Game/Main.cs
+++ src_gui/Assets/Scripts/Game/Main.cs
@@ -26,7 +26,6 @@ public class Ressources {
         mendiane = int.Parse(ressources[7]);
         phiras = int.Parse(ressources[8]);
         thystame = int.Parse(ressources[9]);
-        Debug.Log("Updated " + x + " " + y);
     }
 }
 
@@ -50,40 +49,107 @@ public class Map {
     public int width;
     public int height;
     public List<List<Tiles>> tiles;
+    public List<List<GameObject>> tiles_obj;
     public List<Team> teams = new List<Team>();
     public int time_unit;
 }
 
 public class Main : MonoBehaviour
 {
-    public static Map map = new Map();
-    public GameObject parent;
-    private string response;
-
+    public GameObject foodPrefab;
+    public GameObject linematePrefab;
+    public GameObject deraumerePrefab;
+    public GameObject siburPrefab;
+    public GameObject mendianePrefab;
+    public GameObject phirasPrefab;
+    public GameObject thystamePrefab;
+    public GameObject playerPrefab;
+    public GameObject eggPrefab;
     public GameObject tilePrefab;
+    public GameObject parent;
+    public static Map map = new Map();
+    private string response;
+    private static List<string> ressources_name = new List<string>{"food", "linemate", "deraumere", "sibur", "mendiane", "phiras", "thystame"};
+    enum Ressources_type { food, linemate, deraumere, sibur, mendiane, phiras, thystame };
+
 
     public int nb_teams = 0;
 
     float tileOffset = 1.45f;
 
+    private List<GameObject> getRessourcesOnTile(GameObject tile, Ressources_type type)
+    {
+        List<GameObject> ressources = new List<GameObject>();
+
+        foreach (Transform child in tile.transform.GetComponentInChildren<Transform>()) {
+            if (child.gameObject.tag == ressources_name[(int)type]) {
+                ressources.Add(child.gameObject);
+            }
+        }
+        return ressources;
+    }
+
+    private void SpawnRessources(GameObject tile, GameObject prefab, Ressources_type type)
+    {
+        Vector3 randPosInTile = new Vector3(Random.Range(-tileOffset, tileOffset), 0, Random.Range(-tileOffset, tileOffset));
+        string tag = ressources_name[(int)type];
+
+        GameObject ressource = Instantiate(
+            prefab,
+            tile.transform.position + randPosInTile,
+            Quaternion.identity
+        ) as GameObject;
+        ressource.tag = tag;
+        ressource.transform.parent = tile.transform;
+    }
+
+    private void UpdateRessources(int x, int y, GameObject prefab, int nbr, Ressources_type type)
+    {
+        GameObject tile = map.tiles_obj[x][y];
+        List<GameObject> pref_Ressources = getRessourcesOnTile(tile, type);
+
+        if (pref_Ressources.Count < nbr) {
+            for (int i = pref_Ressources.Count; i < nbr; i++) {
+                SpawnRessources(tile, prefab, type);
+            }
+        } else if (pref_Ressources.Count > nbr) {
+            for (int i = pref_Ressources.Count; i > nbr; i--) {
+                Destroy(pref_Ressources[i - 1]);
+                pref_Ressources.RemoveAt(0);
+            }
+        }
+    }
+
+    private void UpdateTiles(int x, int y, int food, int linemate, int deraumere, 
+    int sibur, int mendiane, int phiras, int thystame)
+    {
+        if (x < 0 || y < 0 || x >= map.width || y >= map.height)
+            return;
+        Debug.Log("Updating tile : niglo : " + x + "," + y);
+        UpdateRessources(x, y, foodPrefab, food, Ressources_type.food);
+        UpdateRessources(x, y, linematePrefab, linemate, Ressources_type.linemate);
+        UpdateRessources(x, y, deraumerePrefab, deraumere, Ressources_type.deraumere);
+        UpdateRessources(x, y, siburPrefab, sibur, Ressources_type.sibur);
+        UpdateRessources(x, y, mendianePrefab, mendiane, Ressources_type.mendiane);
+        UpdateRessources(x, y, phirasPrefab, phiras, Ressources_type.phiras);
+        UpdateRessources(x, y, thystamePrefab, thystame, Ressources_type.thystame);
+    }
+
     private void CreateTileMap()
     {
+        map.tiles_obj = new List<List<GameObject>>();
         map.tiles = new List<List<Tiles>>();
         for (int i = 0; i <= map.height; i++)
         {
+            map.tiles_obj.Add(new List<GameObject>());
             map.tiles.Add(new List<Tiles>());
             for (int j = 0; j <= map.width; j++)
             {
                 map.tiles[i].Add(new Tiles());
                 map.tiles[i][j].content = new Ressources();
-                map.tiles[i][j].content.food = i;
-            }
-        }
-        for (int x = 0; x < map.width; x++) {
-            for (int z = 0; z < map.height; z++) {
-                GameObject Temp = Instantiate(tilePrefab);
-                Temp.transform.position = new Vector3(x * tileOffset, 0, z * tileOffset);
-                SetTileInfo(Temp, x, z);
+                map.tiles_obj[i].Add(Instantiate(tilePrefab));
+                map.tiles_obj[i][j].transform.position = new Vector3(i * tileOffset, 0, j * tileOffset);
+                SetTileInfo(map.tiles_obj[i][j], i, j);
             }
         }
     }
@@ -112,7 +178,19 @@ public class Main : MonoBehaviour
             nb_teams++;
         }
         if (cmd.StartsWith("bct ")) {
-            map.tiles[int.Parse(content[1])][int.Parse(content[2])].content.update(cmd);
+            UpdateTiles(
+                int.Parse(content[1]),
+                int.Parse(content[2]),
+                int.Parse(content[3]),
+                int.Parse(content[4]),
+                int.Parse(content[5]),
+                int.Parse(content[6]),
+                int.Parse(content[7]),
+                int.Parse(content[8]),
+                int.Parse(content[9])
+            );
+            Debug.Log("Updated tile : " + content[1] + " " + content[2]);
+            // map.tiles[int.Parse(content[1])][int.Parse(content[2])].content.update(cmd);
         }
         if (cmd.StartsWith("sgt ")) {
             map.time_unit = int.Parse(content[1]);
@@ -123,10 +201,16 @@ public class Main : MonoBehaviour
         if (!NetworkManager.connected)
             throw new System.Exception("Error disconnected");
         try {
-            string cmd = NetworkManager.ReadServer();
-            if (!string.IsNullOrEmpty(cmd) && (cmd != "" || cmd != "WELCOME"))
-                HandleCommand(cmd);
-        } catch { }
+            NetworkManager.WriteServer("mct");
+            if (NetworkManager.stream.DataAvailable) {
+                Debug.Log("RAAAAAAAAAAAAAAAAAAAAAHH");
+                string cmd = NetworkManager.ReadServer();
+                if (!string.IsNullOrEmpty(cmd) && (cmd != "" || cmd != "WELCOME"))
+                    HandleCommand(cmd);
+            }
+        } catch (System.Exception e) { 
+            Debug.Log("Exception : " + e.Message);
+        }
     }
 
     void Start()
@@ -139,8 +223,6 @@ public class Main : MonoBehaviour
 
             Debug.Log("Connected");
             NetworkManager.WriteServer("msz");
-            NetworkManager.WriteServer("tna");
-            NetworkManager.WriteServer("sgt");
         } else {
             Debug.Log("Not connected");
         }
