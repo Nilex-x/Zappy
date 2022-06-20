f4d4aa5461f32424404c4f5d6f00b2ce3ff75be5
joris <joris.vey@epitech.eu>
2022-06-20 15:13:09 +0200

    [add] implement incantation

--- src_client/ia/main.py
+++ src_client/ia/main.py
@@ -219,6 +219,7 @@ class clientIA:
             self.toSend.put("Forward")
         else:
             self.toSend.put("Take " + ressource)
+            self.ressources[ressource] += 1
             self.toSend.put("Broadcast inventory " + ressource)
         return 0
 
@@ -291,7 +292,13 @@ class clientIA:
     def actionAi(self):
         if self.toSend.empty():
             if self.cmds.empty() and self.currentCmd == "Nothing":
-                action = input("Input: ")
+                action = "Look"
+                if checkRessourceForLevel(self.lvl, self.ressources) == None:
+                    for i in range(1, 7):
+                        for j in range (0, self.ressources[ressources[i-1]]):
+                            if upLvl[self.lvl-1][ressources[i-1]] != 0:
+                                self.toSend.put("Set " + ressources[i-1])
+                    action = "Incantation"
                 if (action == "wait"):  #temp
                     return action       #temp
                 self.toSend.put(action)
