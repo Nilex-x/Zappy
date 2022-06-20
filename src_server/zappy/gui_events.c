0fdedd559caf91c0b04315911e128c95a098aac3
Antoine Cadeau <antoine.cadeau@epitech.eu>
2022-06-20 15:13:29 +0200

    [up] gui_events* : done

--- src_server/zappy/gui_events.c
+++ src_server/zappy/gui_events.c
@@ -21,7 +21,6 @@ void new_player_connect(trantorians_t *t)
             c->data_send = add_send(c->data_send, str);
     }
     free(str);
-    return 0;
 }
 
 void gui_connect_new_player(client_t *gui, zappy_data_t *data)
@@ -34,7 +33,6 @@ void gui_connect_new_player(client_t *gui, zappy_data_t *data)
         gui->data_send = add_send(gui->data_send, str);
     }
     free(str);
-    return 0;
 }
 
 void expulsion_message(trantorians_t *t)
@@ -50,7 +48,6 @@ void expulsion_message(trantorians_t *t)
             c->data_send = add_send(c->data_send, str);
     }
     free(str);
-    return 0;
 }
 
 void broadcast_message(trantorians_t *t, char **args)
@@ -69,7 +66,6 @@ void broadcast_message(trantorians_t *t, char **args)
             c->data_send = add_send(c->data_send, str);
     }
     free(str);
-    return 0;
 }
 
 void ressource_dropping(trantorians_t *t, int obj)
@@ -85,76 +81,4 @@ void ressource_dropping(trantorians_t *t, int obj)
             c->data_send = add_send(c->data_send, str);
     }
     free(str);
-    return 0;
-}
-
-void ressource_collecting(trantorians_t *t, int obj)
-{
-    char *str = NULL;
-    client_t *client = t->client;
-
-    asprintf(&str, "pgt %d %d\n", t->client->socket, obj);
-    while (client->prev)
-        client = client->prev;
-    for (client_t *c = client; c; c = c->next) {
-        if (c->is_gui)
-            c->data_send = add_send(c->data_send, str);
-    }
-    free(str);
-    return 0;
-}
-
-void death_of_a_player(trantorians_t *t)
-{
-    char *str = NULL;
-    client_t *client = t->client;
-
-    asprintf(&str, "pdi %d\n", t->client->socket);
-    while (client->prev)
-        client = client->prev;
-    for (client_t *c = client; c; c = c->next) {
-        if (c->is_gui)
-            c->data_send = add_send(c->data_send, str);
-    }
-    free(str);
-    return 0;
-}
-
-void end_of_game(team_t *team, zappy_data_t *data)
-{
-    char *str = NULL;
-    client_t *client = data->trants->client;
-
-    asprintf(&str, "seg %s\n", team->name);
-    while (client->prev)
-        client = client->prev;
-    for (client_t *c = client; c; c = c->next) {
-        if (c->is_gui)
-            c->data_send = add_send(c->data_send, str);
-    }
-    free(str);
-    return 0;
-}
-
-void message_from_server(client_t *cli, char *str)
-{
-    client_t *client = cli;
-
-    while (client->prev)
-        client = client->prev;
-    for (client_t *c = client; c; c = c->next) {
-        if (c->is_gui)
-            c->data_send = add_send(c->data_send, str);
-    }
-    return 0;
-}
-
-void unknown_gui_command(client_t *cli)
-{
-    cli->data_send = add_send(cli->data_send, "suc\n");
-}
-
-void command_parameter(client_t *cli)
-{
-    cli->data_send = add_send(cli->data_send, "sbp\n");
 }
---
+++ src_server/zappy/gui_events2.c
@@ -0,0 +1,70 @@
+/*
+** EPITECH PROJECT, 2022
+** Zappy
+** File description:
+** gui_events2
+*/
+
+#include "server.h"
+
+void ressource_collecting(trantorians_t *t, int obj)
+{
+    char *str = NULL;
+    client_t *client = t->client;
+
+    asprintf(&str, "pgt %d %d\n", t->client->socket, obj);
+    while (client->prev)
+        client = client->prev;
+    for (client_t *c = client; c; c = c->next) {
+        if (c->is_gui)
+            c->data_send = add_send(c->data_send, str);
+    }
+    free(str);
+}
+
+void death_of_a_player(trantorians_t *t)
+{
+    char *str = NULL;
+    client_t *client = t->client;
+
+    asprintf(&str, "pdi %d\n", t->client->socket);
+    while (client->prev)
+        client = client->prev;
+    for (client_t *c = client; c; c = c->next) {
+        if (c->is_gui)
+            c->data_send = add_send(c->data_send, str);
+    }
+    free(str);
+}
+
+void end_of_game(team_t *team, zappy_data_t *data)
+{
+    char *str = NULL;
+    client_t *client = data->trants->client;
+
+    asprintf(&str, "seg %s\n", team->name);
+    while (client->prev)
+        client = client->prev;
+    for (client_t *c = client; c; c = c->next) {
+        if (c->is_gui)
+            c->data_send = add_send(c->data_send, str);
+    }
+    free(str);
+}
+
+void message_from_server(client_t *cli, char *str)
+{
+    client_t *client = cli;
+
+    while (client->prev)
+        client = client->prev;
+    for (client_t *c = client; c; c = c->next) {
+        if (c->is_gui)
+            c->data_send = add_send(c->data_send, str);
+    }
+}
+
+void unknown_gui_command(client_t *cli)
+{
+    cli->data_send = add_send(cli->data_send, "suc\n");
+}
