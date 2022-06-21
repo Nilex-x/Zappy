4ef3db17b9707816dd6c156f3eae00626d695fcf
nilexx <nicolas.moreau@epitech.eu>
2022-06-21 10:14:59 +0200

    [up] src_server/server/handle_communication.c : remove free_array causing segfault

--- src_server/server/handle_communication.c
+++ src_server/server/handle_communication.c
@@ -81,7 +81,6 @@ void do_action(server_t *info)
         if (act && time.tv_nsec <= 0 && time.tv_sec <= 0) {
             act->action(temp->client, act->args, info->data);
             temp->action = act->next;
-            free_array(act->args);
             free(act);
         } else if (act != NULL)
             act->time_left = time;
