808f30c2fc43cb5a2e9a8c66ab2b82cf3a9f6e86
Jules <jules.cibien@epitech.eu>
2022-06-24 20:27:19 +0200

    [up] Fix player orientation & bad index input

--- src_gui/Assets/Scripts/Game/Main.cs
+++ src_gui/Assets/Scripts/Game/Main.cs
@@ -314,10 +314,6 @@ public class Main : MonoBehaviour
                 map.teams[i_team].players.Add(new Player());
                 map.teams[i_team].players[map.teams[i_team].nb_players].playerTag = int.Parse(playerTag[1]);
                 map.teams[i_team].players[map.teams[i_team].nb_players].orientation = int.Parse(content[4]);
-                if (int.Parse(content[4]) == 2)
-                    map.teams[i_team].players[map.teams[i_team].nb_players].orientation = 4;
-                if (int.Parse(content[4]) == 4)
-                    map.teams[i_team].players[map.teams[i_team].nb_players].orientation = 2;
                 map.teams[i_team].players[map.teams[i_team].nb_players].level = int.Parse(content[5]);
                 map.teams[i_team].players[map.teams[i_team].nb_players].content = new Ressources();
                 map.teams[i_team].players[map.teams[i_team].nb_players].content.x = int.Parse(content[2]);
@@ -327,8 +323,15 @@ public class Main : MonoBehaviour
                 map.teams[i_team].playersObj[map.teams[i_team].nb_players].transform.position =
                     new Vector3(map.teams[i_team].players[map.teams[i_team].nb_players].content.x * TileOffset,
                         0.6f, map.teams[i_team].players[map.teams[i_team].nb_players].content.y * TileOffset);
-                map.teams[i_team].playersObj[map.teams[i_team].nb_players].transform.eulerAngles =
-                    new Vector3(0, 90 * (map.teams[i_team].players[map.teams[i_team].nb_players].orientation - 1), 0);
+               if (map.teams[i_team].players[map.teams[i_team].nb_players].orientation == 1)
+                        map.teams[i_team].playersObj[map.teams[i_team].nb_players].transform.eulerAngles =
+                            new Vector3(0, -90, 0);
+                    else if (map.teams[i_team].players[map.teams[i_team].nb_players].orientation == 3)
+                        map.teams[i_team].playersObj[map.teams[i_team].nb_players].transform.eulerAngles =
+                            new Vector3(0, 90, 0);
+                    else
+                        map.teams[i_team].playersObj[map.teams[i_team].nb_players].transform.eulerAngles =
+                            new Vector3(0, 90 * (map.teams[i_team].players[map.teams[i_team].nb_players].orientation), 0);
                 map.teams[i_team].nb_players++;
                 break;
             }
@@ -343,15 +346,18 @@ public class Main : MonoBehaviour
                     map.teams[i_team].players[i_player].content.x = int.Parse(content[2]);
                     map.teams[i_team].players[i_player].content.y = int.Parse(content[3]);
                     map.teams[i_team].players[i_player].orientation = int.Parse(content[4]);
-                    if (int.Parse(content[4]) == 2)
-                        map.teams[i_team].players[map.teams[i_team].nb_players].orientation = 4;
-                    if (int.Parse(content[4]) == 4)
-                        map.teams[i_team].players[map.teams[i_team].nb_players].orientation = 2;
                     map.teams[i_team].playersObj[i_player].transform.position =
                         new Vector3(map.teams[i_team].players[i_player].content.x * TileOffset,
                             0.6f, map.teams[i_team].players[i_player].content.y * TileOffset);
-                    map.teams[i_team].playersObj[i_player].transform.eulerAngles =
-                        new Vector3(0, 90 * (map.teams[i_team].players[i_player].orientation - 1), 0);
+                    if (map.teams[i_team].players[i_player].orientation == 1)
+                        map.teams[i_team].playersObj[i_player].transform.eulerAngles =
+                            new Vector3(0, -90, 0);
+                    else if (map.teams[i_team].players[i_player].orientation == 3)
+                        map.teams[i_team].playersObj[i_player].transform.eulerAngles =
+                            new Vector3(0, 90, 0);
+                    else
+                        map.teams[i_team].playersObj[i_player].transform.eulerAngles =
+                            new Vector3(0, 90 * (map.teams[i_team].players[i_player].orientation), 0);
                     break;
                 }
             }
